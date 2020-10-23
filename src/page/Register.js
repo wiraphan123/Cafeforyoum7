23699f4d49c9f8f1af1f29966fd513aa1e78d6f7
pondatikan001 <pondatikan01@gmail.com>
2020-10-23 16:37:11 +0700

    Add/page/register

---
+++ src/page/Register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Register/Navbar'
+
+function Register() {
+  return (
+    <div className="container">
+      <Navbar />
+    </div>
+  )
+}
+
+export default Register
