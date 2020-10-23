b21a00cc1c390bab97698c1e7edbf1076115a91f
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-10-23 21:11:57 +0700

    edit Home

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
-import backgroundImage from '../assets/jeehoi.jpg'
+import backgroundImage from '../assets/jessica-arends-8saVYOMHFzU-unsplash.jpg'
 import BackgroundBlock from '../components/BackgroundBlock'
 import { Title, Subtitle } from '../components/Typography'
 
 const Button = styled.button`
 padding: 1rem .5rem;
-background-color:transperent;
+background-color:transparent;
 border: #fff solid 1px;
 border-radius:8px;
 margin:1rem;
@@ -14,20 +14,33 @@ color: #fff ;
 cursor:pointer;
 transition:background-color .3s;
 
-text-transform:uppercase;
+text-transform: uppercase;
+
 
 &:hover{
-  background-color:#fff;
-  color:#000;
+  background-color: #fff;
+  color: #cf7b7e;
 }
 `
-
+ 
 const Content = styled.div`
-  padding:8rem;
+padding: 8rem;
 `
 
-function Home () {
+function Home() {
   return (
+    <>
+      <BackgroundBlock src={backgroundImage}>
+      <Title> Cafe For You </Title>
+          <Subtitle> We hope you will find a good coffee shop in our website </Subtitle>
+          <Button as='a' href='#content'>Review </Button>
+        </BackgroundBlock>
 
+        <Content id='content'>
+        
+        
+        </Content>
+      </>
   )
-  
+}
+export default Home
