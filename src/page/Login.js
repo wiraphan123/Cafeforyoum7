36b73fc0717c9d4fb607a85cebaaddb517aa2086
pondatikan001 <pondatikan01@gmail.com>
2020-10-23 16:35:25 +0700

    Add/page/login

---
+++ src/page/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from "../components/Login/Navbar"
+
+function Login() {
+    return (     
+        <div className="login-container">
+            <Navbar />
+        </div>   
+    );
+}
+
+export default Login;
