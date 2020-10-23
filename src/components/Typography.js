91d2888f9e08d9a452b5064897534b65d6f3d7ea
pondatikan001 <pondatikan01@gmail.com>
2020-10-23 16:50:55 +0700

    Add/components/typography

---
+++ src/components/Typography.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+ export const Text = styled.h1`
+  width: 100%;
+  display: flex;
+  /* justify-content:center; */
+  /* align-items: center; */
+  padding:1rem;
+  color:#FFFFFF;
+  font-size:3rem;
+ 
+  /* margin:0rem 0rem 0rem 4rem; */
+ `
+export const Logo = styled.div`
+padding: 1rem;
+display:flex;
+flex-direction:row;
+align-items: center;
+/* margin:0rem 4rem 0rem 0rem; */
+`
+
+export const TextFooter = styled.span`
+width: 100%;
+display: flex;
+ justify-content:center; 
+align-items: center; 
+padding:1rem;
+color:#FFFFFF;
+font-size:2rem;
+/* margin:0rem 0rem 0rem 4rem; */
+`
