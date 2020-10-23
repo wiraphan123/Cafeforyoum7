407995da726f700712dba6f8c24dd79a85417766
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-10-23 21:20:53 +0700

    edit Typography.js

--- src/components/Typography.js
+++ src/components/Typography.js
@@ -1,31 +1,13 @@
 import styled from 'styled-components'
 
- export const Text = styled.h1`
-  width: 100%;
-  display: flex;
-  /* justify-content:center; */
-  /* align-items: center; */
-  padding:1rem;
-  color:#FFFFFF;
-  font-size:3rem;
- 
-  /* margin:0rem 0rem 0rem 4rem; */
- `
-export const Logo = styled.div`
-padding: 1rem;
-display:flex;
-flex-direction:row;
-align-items: center;
-/* margin:0rem 4rem 0rem 0rem; */
+export const Text = styled.span`
+color: #fff;
+margin: 1rem;
 `
 
-export const TextFooter = styled.span`
-width: 100%;
-display: flex;
- justify-content:center; 
-align-items: center; 
-padding:1rem;
-color:#FFFFFF;
-font-size:2rem;
-/* margin:0rem 0rem 0rem 4rem; */
+export const Title = styled(Text)`
+font-size: 4rem;
+`
+export const Subtitle = styled(Text)`
+font-size: 2rem;
 `
