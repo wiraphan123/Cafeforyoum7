fc16e370410ed82c30236a0e157453149d25918a
pondatikan001 <pondatikan01@gmail.com>
2020-10-23 16:42:18 +0700

    Add/page/review

---
+++ src/page/Review.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from "../components/Review/Navbar"
+
+function Review() {
+    return (     
+        <div >
+            <Navbar />
+        </div>   
+    );
+}
+
+export default Review;
