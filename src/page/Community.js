c199f9c487b4a65368bf9bacfba795a4a21bc2f9
pondatikan001 <pondatikan01@gmail.com>
2020-10-23 16:40:39 +0700

    Add/page/community

---
+++ src/page/Community.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from "../components/Community/Navbar"
+
+function Community() {
+    return (     
+        <div >
+            <Navbar />
+        </div>   
+    );
+}
+
+export default Community;
